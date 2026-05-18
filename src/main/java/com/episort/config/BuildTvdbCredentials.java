bfda5e507d6cc1e5744e0624fbbb0476add85a86
KisukeSaama <KisukeSaama@users.noreply.github.com>
2026-05-18 19:47:55 +0200

    feat: add BuildTvdbCredentials class with API key

---
+++ src/main/java/com/episort/config/BuildTvdbCredentials.java
@@ -0,0 +1,8 @@
+package com.episort.config;
+
+final class BuildTvdbCredentials {
+    static final String API_KEY = "b32f8726-8a33-4564-9c8c-1d8df5287126";
+
+    private BuildTvdbCredentials() {
+    }
+}
